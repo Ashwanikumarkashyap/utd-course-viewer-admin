705b1cda837d7532fe4bb7df0df34490ec98bb93
Ashwani <ashwanikumarkashyap22@gmail.com>
2020-03-07 22:27:38 -0600

    title-alignment-left

--- public/script/listeners.js
+++ public/script/listeners.js
@@ -31,9 +31,19 @@ firebase.auth().onAuthStateChanged(function(user_) {
         programRef.child('courses').on('child_removed', function(childSnapshot) {
             $(document.getElementById(childSnapshot.key)).remove();
         });
+        
+        // update UI
+        document.getElementById("login_block_wrapper").style.display = "none";
 
         // fetch the course data once and update UI
         programRef.once("value", snap => {
+
+            document.getElementById("logout_btn").style.display = "block";
+            document.getElementById("title_block").style.justifyContent = "left";
+            document.getElementById("course_grid").style.display = "flex";
+            document.getElementById("search_block").style.display = "flex";
+            document.getElementById("title").innerHTML = "Courses Offered";
+
             program = snap.val();
             sortedCourses = sortCourses(program)
             createCourseGrid(sortedCourses);
@@ -53,13 +63,6 @@ firebase.auth().onAuthStateChanged(function(user_) {
             }        
         });
         
-        // update UI
-        document.getElementById("logout_btn").style.display = "block";
-        document.getElementById("course_grid").style.display = "flex";
-        document.getElementById("search_block").style.display = "flex";
-        document.getElementById("title").innerHTML = "Courses Offered";
-        document.getElementById("login_block_wrapper").style.display = "none";
-        
     } else {
         // no user is signed in
 
@@ -71,6 +74,7 @@ firebase.auth().onAuthStateChanged(function(user_) {
         }
 
         // update the UI
+        document.getElementById("title_block").style.justifyContent = "center";
         document.getElementById("logout_btn").style.display = "none";
         document.getElementById("about_block").style.display = "none";
         document.getElementById("black_overlay").style.display = "none";
